b71e0933fd3d0d2a77a8275eb875114bf4e516bb
Kenn Brodhagen <kennbrodhagen@gmail.com>
2012-12-05 19:43:58 -0500

    Initial Load

---
+++ GameOfLife/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GameOfLife
+//
+//  Created by Kenn Brodhagen on 12/5/12.
+//  Copyright (c) 2012 Hipsware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
