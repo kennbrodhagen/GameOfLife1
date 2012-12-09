b4f166900237e34a3a5e9cc7b436e00788a8dd9e
Kenn Brodhagen <kennbrodhagen@gmail.com>
2012-12-09 02:21:37 -0500

    Baseline failing test.

---
+++ UT-GameOfLife/GameTest.m
@@ -0,0 +1,22 @@
+//
+//  GameTest.m
+//  GameOfLife
+//
+//  Created by Kenn Brodhagen on 12/9/12.
+//  Copyright (c) 2012 Hipsware LLC. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+
+@interface GameTest : GHTestCase
+
+@end
+
+@implementation GameTest
+
+-(void) test_Runs
+{
+    GHFail(nil);
+}
+
+@end
